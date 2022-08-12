b6435fb3efdba9f978850a80ba57235b00d5fd66
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-08-12 22:12:23 +0800

    Initial major commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
+
+// message
+client.on("message", function(message){
+    console.log(`message has been created -> ${message}`);
+});
