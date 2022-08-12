de6508458dc6c8dffb15f5c0bb49976d35eddb15
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-08-13 02:00:06 +0800

    test

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const { token } = require("./config.json");
-const rest = new REST({ version: '10' }).setToken(token);
-
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-];
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands("1007571386128158750"), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
-client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("interactionCreate", async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-
-	if (interaction.commandName === "ping") {
-		await interaction.reply("Pong!");
-	}
-});
+const { token, FILTER_LIST } = require("./config.json");
 
 client.login(token);
+
+client.on("message", async (message) => {
+	message.channel.sendMessage("GET THAT THE FUCK OUT OF HERE");
+});
