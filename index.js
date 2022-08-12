c5f674c7e37a1c425f2dcffcacac3edb390729a1
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-08-13 01:21:42 +0800

    Test

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-// Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-
-// Create a new client instance
+const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { token } = require("./config.json");
+const rest = new REST({ version: '10' }).setToken(token);
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands("1007571386128158750"), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === "ping") {
+		await interaction.reply("Pong!");
+	}
 });
 
 client.login(token);
-
-// message
-client.on('messageCreate', () => {
-	console.log("Lmao");
-});
