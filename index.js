b244febcdefbd41223f718a577b3737739afa9cc
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-08-15 00:05:11 +0800

    Add slash command

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const client = new Client({ intents: ["Guilds", "GuildMessages", "MessageContent"] });
 const { token } = require("./config.json");
 const { triggers } = require("./blacklist.json");
 
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 // random messages
 const randomGreeting = [
@@ -90,6 +103,24 @@ client.on("messageCreate", async (message) => {
 			.setFooter({ text: "We’re in this together 💪" });
 
 		message.channel.send({ embeds: [hotlineEmbed] });
+	}
+});
+
+client.on("interactionCreate", async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: "There was an error while executing this command!",
+			ephemeral: true,
+		});
 	}
 });
 
